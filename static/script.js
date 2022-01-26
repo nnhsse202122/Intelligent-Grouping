0ec3c42f7d4008c7a0e6113a5ce6cd892900ff56
Ryan Berwick <rberwick@stu.naperville203.org>
2022-01-26 14:23:24 -0600

    Continue working on dropdown

--- static/script.js
+++ static/script.js
@@ -22,11 +22,6 @@ startLoad()
 function setState(mode, info={}) {
   state.mode = mode
   state.info = info
-  console.log(`STATE CHANGE TO ${mode}, INFO:${info}`)
-  for(const prop in info){
-    console.log(prop)
-    
-  }
 }
 
 function resetApp() {
