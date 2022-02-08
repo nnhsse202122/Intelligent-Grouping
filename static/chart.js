caed0fc96d8a56b8ae7eb4b6c1d18843c8e64332
Ryan Berwick <rberwick@stu.naperville203.org>
2022-02-08 14:19:47 -0600

    Start work on retrieving and formatting groups

--- static/chart.js
+++ static/chart.js
@@ -3,4 +3,26 @@ function seatingChart(grouping){
     statusTitle.innerText = "Seating Chart"
     switchSection(seatingChartSection)
     setState(7, {id: state.info.id, groupingId: grouping.id, currentGroup:grouping})
+    getGroups(grouping)
+}
+
+function getGroups(grouping){
+    //making deep copy of groups for names
+    const nameGroups = [];
+    for(const group of grouping.groups){
+        nameGroups.push(...group)
+    }
+    console.log(nameGroups)
+    //changing from ids to names
+    for(const student of classes[state.info.id].obj.students){
+        for(const group of nameGroups){
+            for(let i = 0; i < group.length; i++){
+                //console.log(`STU: ${student.id} vs GROUP: ${group[i]}`)
+                if(student.id == group[i]){
+                    group[i] = student; //changes it to a student OBJECT (not string)
+                }
+            }
+        }
+    }
+    console.log(nameGroups);
 }
