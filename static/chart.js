a620a38791243632700ac37cd7704a05f329cc5e
anthonyjk <anthonyjk@stu.naperville203.org>
2022-02-08 14:20:06 -0600

    Started on CSSGrid for seating chart

--- static/chart.js
+++ static/chart.js
@@ -4,3 +4,26 @@ function seatingChart(grouping){
     switchSection(seatingChartSection)
     setState(7, {id: state.info.id, groupingId: grouping.id, currentGroup:grouping})
 }
+
+window.addEventListener("DOMContentLoaded", function() {
+    let boxes = document.querySelectorAll(".box");
+  
+    Array.from(boxes, function(box) {
+      box.addEventListener("click", function() {
+        alert(this.classList[1]);
+        // On box click function here for when we need to move groups to click location
+      });
+    });
+  });
+
+function createGrid()
+{
+    for(let row = 0; row < 6; row++) {
+        for(let col = 0; col < 6; col++) {
+            let div = document.createElement("div");
+            let place = div.classList.add(`box [${row}][${col}]`);
+            div.appendChild(place);
+            grid.appendChild(div); // Trying to append div to grid
+        }
+    }
+}
