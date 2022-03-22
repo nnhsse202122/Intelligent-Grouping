5f0ec42a16165cbc52cd516bf8622bc5c75d6806
anthonyjk <anthonyjk@stu.naperville203.org>
2022-03-22 14:19:52 -0500

    started on dropdown

--- static/responses.js
+++ static/responses.js
@@ -6,25 +6,9 @@ function showResponses() {
 }
 
 function somethingOrOther(){
-    console.log("t0")
-  //for(let student in group)
-  // Find a way to loop through the students in a class individually and call .preferences on each and print it out.
-  /*for(pref in classes[state.info.id].obj.preferences) {
-    console.log("t1")
-    console.log(pref)
-    console.log("t2")
-  }*/
-  console.log(classes[state.info.id])
-  console.log(classes[state.info.id].obj)
-  console.log(classes[state.info.id].obj.students)
-  console.log(classes[state.info.id].obj.student)
-  console.log(classes[state.info.id].obj.preferences)
-  console.log(classes[state.info.id].obj.preference)
-  for(let i = 0; i < 19; i++/*let student in classes[state.info.id].obj.students*/) {
+  for(let i = 0; i < classes[state.info.id].obj.students.length; i++) {
     
     console.log(classes[state.info.id].obj.students[i])
     console.log(classes[state.info.id].obj.students[i].preferences)
-    console.log(classes[state.info.id].obj.students[i].preferences.studentLike[0])
-    console.log(classes[state.info.id].obj.students[i].preferences.studentDislike[0])
   }
 }
