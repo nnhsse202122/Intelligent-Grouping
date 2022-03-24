a9b25c76e8bcc65babe5f188cd6aaa3eb1a3a88c
anthonyjk <anthonyjk@stu.naperville203.org>
2022-03-24 13:59:19 -0500

    dropdown working but not efficent, will change

--- static/responses.js
+++ static/responses.js
@@ -12,3 +12,21 @@ function somethingOrOther(){
     console.log(classes[state.info.id].obj.students[i].preferences)
   }
 }
+
+/***
+ * Adds student names to dropdown menu
+ */
+function adjustDropdown(){
+
+  for(let i = 0; i < classes[state.info.id].obj.students.length; i++) {
+    classes[state.info.id].obj.students[i]
+
+    let student = document.createElement('a');
+    student.setAttribute('href', '#' + classes[state.info.id].obj.students[i].id);
+    student.innerText = classes[state.info.id].obj.students[i].first + " " + classes[state.info.id].obj.students[i].last
+
+    document.getElementById("studentDropdown").appendChild(student)
+  }
+
+  document.getElementById("studentDropdown").classList.toggle("show");
+}
